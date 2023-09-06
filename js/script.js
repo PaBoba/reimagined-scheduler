e02ecd0ba2430b0cec3fd9a40b318d1b9d1050ab
PaBoba <140555335+PaBoba@users.noreply.github.com>
2023-09-06 16:53:34 -0700

    Added button variable for each hour

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var saveBtn = $('.saveBtn');
+let saveBtn9 = $('#hour-9').children('.btn');
+let saveBtn10 = $('#hour-10').children('.btn');
+let saveBtn11 = $('#hour-11').children('.btn');
+let saveData9 = $('#hour-9').children('.description');
 
-saveBtn.on('click', function () {
-  alert('Save');
+saveBtn9.on('click', function () {
+  // alert('Save');
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -17,7 +20,17 @@ saveBtn.on('click', function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  console.log(saveData9.text());
+  
+  if (saveData9) {
+    text = "";
+  alert('Textbox cannot be empty');
+  }
+
+  else (saveData9); {
+    text != "";
+  localStorage.setItem("todo@9", saveData9.text());
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
